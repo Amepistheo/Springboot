f6a6a120c244ba031b7457f7b60c930647300ce7
Amepistheo <kellylhji@gmail.com>
2024-01-12 18:56:33 +0900

    @Configuration과 싱글톤

--- springboot_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ springboot_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,9 +7,9 @@ import hello.core.member.MemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
@@ -22,5 +22,10 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
