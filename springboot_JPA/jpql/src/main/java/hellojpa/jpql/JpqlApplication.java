7cf01499f2be48fccb144649702edee7eefef091
Amepistheo <kellylhji@gmail.com>
2024-08-15 14:43:17 +0900

    벌크 연산

--- springboot_JPA/jpql/src/main/java/hellojpa/jpql/JpqlApplication.java
+++ springboot_JPA/jpql/src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -39,16 +39,14 @@ public class JpqlApplication {
 			member3.setTeam(teamB);
 			em.persist(member3);
 
-			em.flush();
+			// FLUSH 자동 호출
+			int resultCount = em.createQuery("update Member m set m.age = 20").executeUpdate();
+
 			em.clear();
 
-			List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-					.setParameter("username", "회원1")
-					.getResultList();
+			Member findMember = em.find(Member.class, member1.getId());
 
-			for (Member member : resultList) {
-				System.out.println("member = " + member);
-			}
+			System.out.println("findMember.getAge() = " + findMember.getAge());
 
 			tx.commit();
 		} catch (Exception e) {
